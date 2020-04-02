c6502c065f2021879cf93ead69d67ca974790dda
Gabriel Benny <gabby87benny@gmail.com>
2020-04-02 12:34:16 -0500

    Added image view functionality along with assets

--- BJKit/View Controllers/ImageViewController.swift
+++ BJKit/View Controllers/ImageViewController.swift
@@ -9,5 +9,26 @@
 import UIKit
 
 class ImageViewController: UIViewController {
+    // MARK: - View Life Cycle
+
+    @IBOutlet weak var customImageView: UIImageView!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureImageView()
+    }
+    
+    // MARK: - Configuration
+    
+    func configureImageView() {
+        // We want the image to be scaled to the correct aspect ratio within imageView's bounds.
+        customImageView.animationImages = (1...2).map {
+            (UIImage(named: "Flowers_\($0)")!)
+        }
+        customImageView.contentMode = .scaleAspectFit
+        customImageView.animationDuration = 5
+        customImageView.startAnimating()
+        customImageView.isAccessibilityElement = true
+        customImageView.accessibilityLabel = NSLocalizedString("Animated", comment: "")
+    }
 }
