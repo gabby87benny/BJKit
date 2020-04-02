a0c965a9daddbe1a43cd1a78ab51c3cdf31beec9
Gabriel Benny <gabby87benny@gmail.com>
2020-04-02 17:19:30 -0500

    Implemented date picker and text view functionality

--- BJKit/View Controllers/DatePickerController.swift
+++ BJKit/View Controllers/DatePickerController.swift
@@ -9,5 +9,35 @@
 import UIKit
 
 class DatePickerController: UIViewController {
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var dateLabel: UILabel!
     
+    lazy var dateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .short
+        return dateFormatter
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureDatePicker()
+    }
+
+    // MARK: - Configuration
+    func configureDatePicker() {
+        datePicker.datePickerMode = .dateAndTime
+        let now = Date()
+        datePicker.minimumDate = now
+        let sevenDaysFromNow = Calendar.current.date(byAdding: .day, value: 7, to: now)
+        datePicker.maximumDate = sevenDaysFromNow
+        datePicker.minuteInterval = 2
+        datePicker.addTarget(self, action: #selector(updateDatePickerLabel), for: .valueChanged)
+        updateDatePickerLabel()
+    }
+    
+    @objc
+    func updateDatePickerLabel() {
+        dateLabel.text = dateFormatter.string(from: datePicker.date)
+    }
 }
