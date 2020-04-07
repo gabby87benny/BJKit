c960d646b9b01980c37abaa313a11055524d7886
Gabriel Benny <gabby87benny@gmail.com>
2020-04-07 13:25:40 -0500

    implemented page and stack control

--- BJKit/View Controllers/PageControlViewController.swift
+++ BJKit/View Controllers/PageControlViewController.swift
@@ -9,5 +9,38 @@
 import UIKit
 
 class PageControlViewController: UIViewController {
+    @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var colorView: UIView!
     
+    // Colors that correspond to the selected page. Used as the background color for `colorView`.
+    let colors = [
+        UIColor.black,
+        UIColor.systemGray,
+        UIColor.systemRed,
+        UIColor.systemGreen,
+        UIColor.systemBlue,
+        UIColor.systemPink,
+        UIColor.systemYellow,
+        UIColor.systemIndigo,
+        UIColor.systemOrange,
+        UIColor.systemPurple
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configurePageControl()
+        pageControlValueDidChange()
+    }
+
+    func configurePageControl() {
+        pageControl.numberOfPages = colors.count
+        pageControl.currentPage = 2
+        pageControl.pageIndicatorTintColor = UIColor.systemGreen
+        pageControl.currentPageIndicatorTintColor = UIColor.systemPurple
+        pageControl.addTarget(self, action: #selector(PageControlViewController.pageControlValueDidChange), for: .valueChanged)
+    }
+    
+    @objc func pageControlValueDidChange() {
+        colorView.backgroundColor = colors[pageControl.currentPage]
+    }
 }
